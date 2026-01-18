13cc020015e0802afae5a3b88875048482a3036a
Anukul Pandey <62092256+anukulpandey@users.noreply.github.com>
2026-01-18 12:30:54 +0530

    Update Greeter.sol

--- Greeter.sol
+++ Greeter.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Greeter{
+    string public message;
+
+    constructor(){
+        message ="hello world!";
+    }
+
+   function setGreeting(string calldata _greeting) public{
+    message = _greeting;
+    }
+
+    function getGreeting() view external returns(string memory){
+        return message;
+    }
+}
